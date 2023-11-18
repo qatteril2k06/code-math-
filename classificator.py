5a832cf362ae555266ede3368021b13b57e10fe8
fahrenghate <128506486+fahrenghate@users.noreply.github.com>
2023-11-18 11:59:54 +0300

    Update classificator.py

--- classificator.py
+++ classificator.py
@@ -20,7 +20,7 @@ def calculate_idf(term, folder_path):
     for file_name in os.listdir(folder_path):
         path_to_file = os.path.join(folder_path, file_name)
 
-        if os.path.isfile(path_to_file) and file_path.endswith('.txt'):
+        if os.path.isfile(path_to_file) and path_to_file.endswith('.txt'):
             with open(path_to_file, 'r', encoding='UTF8') as file:
                 document = file.read()
                 all_documents.append(document)
@@ -30,31 +30,30 @@ def calculate_idf(term, folder_path):
     return math.log((total_documents + 1) / (document_count_with_term + 1)) + 1
 
 
-def thematizer(pathtofile, topicspath):
-    main_words = dict(sorted(word_counter(pathtofile).items(), key=lambda item: item[1], reverse=True))
-    main_topic_flag = False
-    word_value = 0
-    for filename in os.listdir(topicspath):
-        file_path = os.path.join(topicspath, filename)
-        with open(file_path, 'r', encoding='utf-8') as topic:
+def thematizer(pathtofiles, topicspath):
+    result_file = open('classification.txt', 'w', encoding='utf-8')
+    filenames = next(os.walk(f'{pathtofiles}'), (None, None, []))[2]
+    main_words = word_counter(pathtofiles, filenames)
+    for dict_of_words, namefile in main_words:
+        dict_of_words = dict(sorted(dict_of_words.items(), key=lambda item: item[1], reverse=True))
+        main_topic_flag = False
+        word_value = 0
+        for filename in os.listdir(topicspath):
+            file_path = os.path.join(topicspath, namefile)
             main_counter = 10
             terms_list = list()
-            for i in main_words.keys():
+            for i in dict_of_words.keys():
                 if main_counter != 0:
                     main_counter -= 1
-                    terms_list.append(main_words[i])
+                    terms_list.append(dict_of_words[i])
                 else:
                     break
-            for word in terms_list:
-                if calculate_tf(word, topic.read()) * calculate_idf(word, topicspath) > word_value:
-                    word_value = calculate_tf(word, topic.read()) * calculate_idf(word, topicspath)
-                    main_topic_flag = filename
-    return main_topic_flag
+            with open(file_path, 'r', encoding='utf-8') as topic:
+                main_words = main_words[:11]
+                for word, _ in main_words:
+                    if calculate_tf(word, topic.read()) * calculate_idf(word, topicspath) > word_value:
+                        word_value = calculate_tf(word, topic.read()) * calculate_idf(word, topicspath)
+                        main_topic_flag = filename
+        result_file.write(namefile + '\t' + main_topic_flag + '\n')
 
-
-with open('classification.txt', 'w', encoding='utf-8') as result_file:
-    for name_of_file in os.listdir(path_to_files):
-        file_path = os.path.join(path_to_files, name_of_file)
-        final_theme = thematizer(file_path, path_to_themes)
-        result_file.write(name_of_file + '\t' + final_theme + '\n')
-
+thematizer()
